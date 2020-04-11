2f8310a3296eb79ed641d4f55e53d9da6b3d5263
Milad <moorara@users.noreply.github.com>
2020-04-11 16:50:16 -0400

    Update observe.go

--- observe.go
+++ observe.go
@@ -2,8 +2,6 @@
 // It provides structured logging, metrics, and tracing in one package.
 //
 // Logging:
-// You can either create an instance logger or use the singleton (global) logger.
-// observe.Logger is concurrently safe to be used by multiple goroutines.
 //
 // Metrics:
 //
